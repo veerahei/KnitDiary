737c3ad17a53564d5d08855364394057be4bf53c
Veera <veera.heikkinen95@gmail.com>
2025-03-28 16:25:26 +0200

    User management added

---
+++ knitdiary/src/main/java/knitdiary/knitdiary/web/UserController.java
@@ -0,0 +1,57 @@
+package knitdiary.knitdiary.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import knitdiary.knitdiary.domain.AppUser;
+import knitdiary.knitdiary.domain.AppUserRepository;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    AppUserRepository auRepository;
+
+    // Get list of users
+    // For admin only
+    @GetMapping("/userManagement")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String getUserManagement(Model model) {
+        model.addAttribute("users", auRepository.findAll());
+
+        return "usermanagement";
+    }
+
+    // Edit user
+    @GetMapping("/editUser/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String editUser(@PathVariable("id") Long userId, Model model) {
+
+        // Set the user given in pathvariable to a model, to bring it to edituser view
+        model.addAttribute("user", auRepository.findById(userId));
+
+        return "edituser";
+    }
+
+    // Save edited user
+    @PostMapping("/saveUser")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String saveUser(@ModelAttribute("user") AppUser user) {
+
+        // Set edited attributes to existing user
+        AppUser existingUser = auRepository.findById(user.getUserId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        existingUser.setUsername(user.getUsername());
+        existingUser.setRole(user.getRole());
+
+        auRepository.save(existingUser);
+
+        return "redirect:/userManagement";
+    }
+}
--- knitdiary/src/main/java/knitdiary/knitdiary/web/knitDiaryController.java
+++ knitdiary/src/main/java/knitdiary/knitdiary/web/knitDiaryController.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
